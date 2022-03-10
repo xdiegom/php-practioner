4f5d12eda2a039ab013b853813a9ca537be175a7
Diego Melgar <diego.melgar.sv@gmail.com>
2022-03-09 19:29:19 -0600

    intro to variables - step 3

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
 
-$greeting = 'Hello World!!!';
+// In real life variables will be modified.
 
-echo $greeting;
+$greeting = 'Ahoy';
+$name = 'Diego';
+
+//  Difference between single quotes and double quotes:
+//  ** Double Quotes **
+//  Using double quotes, we can interpolate variables into a string. E.g:
+
+echo "Using double quotes \"\":";
+echo "<br> Hello, my name is $name";
+
+// Also, with double quotes, you can concatenate two or variables
+// using curly braces, which is something that some developers might
+// tend to use
+
+echo "<br><br> Using double quotes with interpolation and curly braces '':<br>";
+echo "{$greeting}, {$name}";
+
+//  ** Single Quotes **
+//  Using single quotes, we can use string concatenation with a dot "." E.g:
+
+echo "<br><br> Using single quotes with concatenation '':";
+echo '<br>' . 'Hello, my name is ' . $name;
